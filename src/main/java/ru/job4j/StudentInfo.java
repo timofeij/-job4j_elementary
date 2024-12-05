cf1cf64b88f9978d130853976ae964d1c59f718f
tim <ageevtimofej05@gmail.com>
2024-12-05 19:12:35 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Timofei Konstantinovich");
+        System.out.println("06.09.2008");
+    }
+}
