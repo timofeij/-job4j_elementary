c1e7b0ef6c9b2aa17f81e3d88b32fdfd765329e4
tim <ageevtimofej05@gmail.com>
2024-12-10 21:55:49 +0300

    4. Методы [#231984]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,9 +1,11 @@
-package ru.job4j;
+package ru.job4;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет мир");
         System.out.println("Hello, world!");
         System.out.println("Привет");
+
+        char tools = 'l';
     }
 }
--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static void main(String[] args){
         System.out.println("Timofei Konstantinovich");
         System.out.println("06.09.2008");
