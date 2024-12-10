339fb87c7f7fa24982e0ce214ff78e5e83152bcd
tim <ageevtimofej05@gmail.com>
2024-12-10 21:12:21 +0300

    t

---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int ten = 10;
+        int two = 2;
+        int tenDivtwo = ten / two;
+        System.out.println(tenDivtwo);
+
+    }
+}
